b6c7cfa849c1e089280b6168c7d880aea7ec788c
hadung <cdevhaxdung@gmail.com>
2018-09-04 16:42:44 +0700

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,22 @@
 class SessionsController < ApplicationController
   def new
+    render "shared/_form_login"
   end
 
   def create
-     user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
     else
       flash[:danger] = 'Invalid email/password combination'
-      render 'login'
+      render 'shared/_form_login'
     end
   end
 
   def destroy
-  	
+  	log_out if logged_in?
+    redirect_to root_url
   end
 end
