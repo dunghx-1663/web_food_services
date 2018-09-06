50d143be4fbc77728689683ea9e895716f6b9330
hadung <cdevhaxdung@gmail.com>
2018-09-06 08:52:20 +0700

    update_profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { maximum: 6 }
+  validates :password, presence: true, length: { maximum: 8 }, allow_nil: true
   
 
   class << self
