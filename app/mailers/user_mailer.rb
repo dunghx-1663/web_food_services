3fcb7dfcf1ac7caafae82f7e453411e732138a50
haxdung <ha.xuan.dung@sun-asterisk.com>
2019-11-27 14:31:56 +0700

    delayed_job_send_mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,6 @@ class UserMailer < ApplicationMailer
   default from: 'dung.ha.xuan97.sun@gmail.com'
 
   def account_activation user
-    binding.pry
     @user = user
     mail(to: @user.email, subject: 'webcome to my website, account activation')
   end
@@ -10,6 +9,11 @@ class UserMailer < ApplicationMailer
   def order_mail user
     @user = user
     mail to: @user.email, subject: "order detail"
+  end
+
+  def notify_delay_job_email user
+    @user = user
+    mail to: @user.email, subject: "notification use delayed job of system, please active account"
   end
 
   def password_reset
